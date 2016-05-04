4fbf7408475a5d85ddb5baecba4a791ec8ef4d66
Gandalf- <austin.voecks@gmail.com>
2016-05-04 10:28:38 -0700

    Reorganize functions

--- bdgraph.py
+++ bdgraph.py
@@ -25,6 +25,7 @@ import sys, getopt, copy
 
 # globals
 nice_line_length = 25
+no_output = False
 
 ''' string : Node '''
 node_dict = {}
@@ -117,6 +118,67 @@ def split_on_nearest_space(word, start):
 
     return word
 
+def file_write(output, string):
+    ''' string, string -> none
+    '''
+    if not no_output:
+        output.write(string)
+    return
+
+def cleanup_input(ordered_node, filename):
+    ordered_elems = sorted(node_order, key=lambda x: int(x[0]))
+
+    with open(filename, 'w') as output:
+        for e in ordered_elems:
+            if e[1] != -1:
+                output.write(format(e[1], " 3d"))
+
+                if e[3].orig_name[0] in special_chars:
+                    output.write(': ' + e[3].orig_name + '\n')
+                else:
+                    output.write(':  ' + e[3].orig_name + '\n')
+            else:
+                output.write("\n")
+
+        output.write("options\n")
+        for e in options:
+            output.write("  " + e + "\n")
+
+        # filter out newlines
+        ordered_elems[:] = filter(lambda x : x[1] != -1, ordered_elems)
+        ordered_copy = copy.deepcopy(ordered_elems)
+
+        output.write("\ndependencies\n")
+
+        # for each element
+        for e in ordered_elems:
+
+            # if it has children
+            if e[3].children:
+                child_count = 0
+                num_children = len(e[3].children)
+                output.write("  ")
+
+                # for each child
+                for child in e[3].children:
+                    number = " "
+
+                    # find the child in the ordered list
+                    for elem in ordered_copy:
+
+                        if elem[3].orig_name == child.orig_name:
+                            number = str(elem[1])
+
+                    if child_count < num_children -1:
+                        output.write(number + ",")
+                        child_count = child_count +1
+
+                    else:
+                        output.write(number + " -> ")
+
+                output.write(str(e[1]) + "\n")
+    return
+
 def get_and_prep_elem(key):
     ''' string -> Node
     retrieves elements from the node_dict dictionary and inserts
@@ -190,7 +252,7 @@ def parse_dependencies(line, output):
             left_node = get_and_prep_elem(clean_str(elem) )
             right_node.add_child(left_node)
 
-            output.write('"' + left_node.name + '" -> "' + 
+            file_write(output, '"' + left_node.name + '" -> "' + 
                          right_node.name + '"' + right_node.action + '\n')
     return
 
@@ -205,23 +267,27 @@ def main(argv):
     found_options = False
 
     # parse args
-    fname = str(argv[0])
-    oname = str(argv[1])
-
-    if not fname or not oname:
-        print('python bdgraph.py <input_file> <output_file>')
+    if len(argv) > 0:
+        fname = str(argv[0])
+    else:
+        print('python bdgraph.py input_file [output_file]')
         sys.exit(1)
 
     # read input
     with open(fname, 'r') as f:
         content = [elem.strip('\n') for elem in f.readlines()]
 
+    if len(argv) > 1:
+        oname = str(argv[1])
+    else:
+        oname = fname + ".dot"
+
     # write output
     with open(oname, 'w') as output:
-        output.write("digraph g{\n")
-        output.write("rankdir=LR;\n")
-        output.write("ratio = fill;\n")
-        output.write("node [style=filled];\n")
+        file_write(output, "digraph g{\n")
+        file_write(output, "rankdir=LR;\n")
+        file_write(output, "ratio = fill;\n")
+        file_write(output, "node [style=filled];\n")
 
         line_num = 0
         elem_num = 1
@@ -283,58 +349,13 @@ def main(argv):
         # repeated, but that's by design
         for key in key_list:
             tmp_node = get_and_prep_elem(key)
-            output.write('"' + tmp_node.name + '"' + tmp_node.action + '\n')
+            file_write(output, '"' + tmp_node.name + '"' + tmp_node.action + '\n')
 
-        output.write("}\n")
+        file_write(output, "}\n")
     
         # clean up the input file?
         if 'cleanup' in options:
-            ordered_elems = sorted(node_order, key=lambda x: int(x[0]))
-
-            with open("rewrite.bdot", 'w') as output:
-                for e in ordered_elems:
-                    if e[1] != -1:
-                        output.write(format(e[1], " 3d") + ':  ' + e[3].orig_name + '\n')
-                    else:
-                        output.write("\n")
-
-                output.write("options\n")
-                for e in options:
-                    output.write("  " + e + "\n")
-
-                # filter out newlines
-                ordered_elems[:] = filter(lambda x : x[1] != -1, ordered_elems)
-                ordered_copy = copy.deepcopy(ordered_elems)
-
-                output.write("\ndependencies\n")
-
-                # for each element
-                for e in ordered_elems:
-
-                    # if it has children
-                    if e[3].children:
-                        child_count = 0
-                        num_children = len(e[3].children)
-                        output.write("  ")
-
-                        # for each child
-                        for child in e[3].children:
-                            number = " "
-
-                            # find the child in the ordered list
-                            for elem in ordered_copy:
-
-                                if elem[3].orig_name == child.orig_name:
-                                    number = str(elem[1])
-
-                            if child_count < num_children -1:
-                                output.write(number + ",")
-                                child_count = child_count +1
-
-                            else:
-                                output.write(number + " -> ")
-
-                        output.write(str(e[1]) + "\n")
+            cleanup_input(node_order, fname)
 
     return
 
